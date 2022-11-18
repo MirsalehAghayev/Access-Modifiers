b866c3aaac14249249da4a18940d3918396d702f
Mirsaleh <mirsalehaa@code.edu.az>
2022-11-18 15:30:39 +0400

    Encapsulation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Student st = new Student();
+            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (var item in st.Grades)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
